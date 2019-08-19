9704e4f1490b5204f16e217513cafe5bf644b39b
Ben Francisco <bpfranci@math.tamu.edu>
2019-08-19 19:40:20 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -58,6 +58,9 @@ def turn(board)
     puts "Please enter 1-9:"
     input = gets.strip()
     index = input_to_index(input)
-    break
+    if valid_move?(board, index)
+      move(board, index)
+      display_board(board)
+      move_accepted = TRUE
   end
 end
